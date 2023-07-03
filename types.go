5c81a2de0efe52b21821d68cf9f04e2224b42a9e
Amonc <amonchowdhury8@gmail.com>
2023-07-03 09:18:38 +0600

    api base done

--- types.go
+++ types.go
@@ -1,20 +1,30 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 type Account struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Number    int64     `json:"number"`
+	Balance   int64     `json:"balance"`
+	createdAt time.Time `json: "created_at"`
+}
+
+type CreateAccountRequest struct {
+	FirstName string `json: "first_name"`
+	LastName  string `json: "last_name"`
 }
 
 func NewAccount(firstName, lastName string) *Account {
 	return &Account{
-		ID:        rand.Intn(100000),
+
 		FirstName: firstName,
 		LastName:  lastName,
 		Number:    int64(rand.Intn(100000)),
+		createdAt: time.Now().UTC(),
 	}
 }
